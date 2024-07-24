86c251aaf6bc13ee60790383482348b24d99c133
Kelmain <neumann.armin79@gmail.com>
2024-07-24 09:13:06 +0200

    add main.js

---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+const mrc = document.querySelector("#mrc");
+const heart = document.querySelector("#heart");
+const diabete = document.querySelector("#diabete");
+const cancer = document.querySelector("#cancer");
+const liver = document.querySelector("#liver");
