f14a564b485c28650220c4737a27806a16400af2
Kelmain <neumann.armin79@gmail.com>
2024-07-24 09:51:02 +0200

    add id, create update value function in js

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,14 @@
-const mrc = document.querySelector("#mrc");
-const heart = document.querySelector("#heart");
-const diabete = document.querySelector("#diabete");
-const cancer = document.querySelector("#cancer");
-const liver = document.querySelector("#liver");
+const mrc = document.getElementById("mrc");
+const heart = document.getElementById("heart");
+const diabete = document.getElementById("diabete");
+const cancer = document.getElementById("cancer");
+const liver = document.getElementById("liver");
+const diseaseId = document.getElementById("diseaseId");
+
+
+
+function updateValue() {
+    let value = this.dattaset.value;
+    diseaseId.setAttribute("value", value);
+    console.log("updateValue");
+}
